81fe8316a3c1a87ffcf9a9dfd85d37266bca3713
jpcubero1 <47186519+jpcubero1@users.noreply.github.com>
2019-01-30 17:34:30 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Hello World!</title>
+    <meta charset="UTF-8">
+    </head>
+    
+    <body lang="es">
+      <?php echo "Hello World!"; ?>
+    </body>
+    
+ </html>
